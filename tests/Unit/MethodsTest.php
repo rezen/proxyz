e910c4597bfb807b8f6be719f2995ddfb04b96bd
Andres Hermosilla <an2dres5m@gmail.com>
2020-08-26 14:12:05 -0700

    Added a couple unit tests

--- tests/Unit/MethodsTest.php
+++ tests/Unit/MethodsTest.php
@@ -2,12 +2,29 @@
 
 namespace Proxyz\Tests\Unit;
 
+use Exception;
 use Proxyz\Tests\UnitTest;
+use function Proxyz\Php\Filesystem\file_get_contents;
 
 class MethodsTest extends UnitTest
 {
-    public function test_add_override()
+    public function test_proxy_add_wrapper()
     {
-        $this->assertGreaterThan(0, 1);
+        \Proxyz\addWrapper('file_get_contents', function($args, $fn) {
+            if ($args[0] === "config.json") {
+                return "{}";
+            }
+            return call_user_func_array($fn, $args);
+        });
+        $this->assertEquals("{}", file_get_contents("config.json"));
+        $this->assertEquals(null, @file_get_contents("xyz.json"));
+    }
+
+    public function test_raises_exception_for_nonexistant_function()
+    {
+        $this->expectException(Exception::class);
+        \Proxyz\addWrapper('fake', function($args, $fn) {
+            return call_user_func_array($fn, $args);
+        });
     }
 }
