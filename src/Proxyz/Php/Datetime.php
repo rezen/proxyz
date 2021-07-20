85e15625cabfb435fead4c72334d4869f16a39a5
Andres Hermosilla <an2dres5m@gmail.com>
2021-07-20 15:25:07 -0700

    Add Datetime functions

---
+++ src/Proxyz/Php/Datetime.php
@@ -0,0 +1,210 @@
+<?php namespace Proxyz\Php\Datetime;
+// This file is dynamically generated ... do not edit 
+
+function checkdate() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_add() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_create() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_create_from_format() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_create_immutable() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_create_immutable_from_format() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_date_set() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_default_timezone_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_default_timezone_set() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_diff() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_format() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_get_last_errors() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_interval_create_from_date_string() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_interval_format() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_isodate_set() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_modify() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_offset_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_parse() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_parse_from_format() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_sub() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_sun_info() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_sunrise() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_sunset() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_time_set() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_timestamp_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_timestamp_set() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_timezone_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function date_timezone_set() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function getdate() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function gettimeofday() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function gmdate() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function gmmktime() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function gmstrftime() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function idate() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function localtime() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function microtime() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function mktime() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function strftime() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function strptime() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function strtotime() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function time() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_abbreviations_list() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_identifiers_list() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_location_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_name_from_abbr() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_name_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_offset_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_open() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_transitions_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function timezone_version_get() {
+  return \Proxyz\callFunction(__FUNCTION__, func_get_args());
+}
+function methods() {
+  return [
+    "checkdate",
+    "date",
+    "date_add",
+    "date_create",
+    "date_create_from_format",
+    "date_create_immutable",
+    "date_create_immutable_from_format",
+    "date_date_set",
+    "date_default_timezone_get",
+    "date_default_timezone_set",
+    "date_diff",
+    "date_format",
+    "date_get_last_errors",
+    "date_interval_create_from_date_string",
+    "date_interval_format",
+    "date_isodate_set",
+    "date_modify",
+    "date_offset_get",
+    "date_parse",
+    "date_parse_from_format",
+    "date_sub",
+    "date_sun_info",
+    "date_sunrise",
+    "date_sunset",
+    "date_time_set",
+    "date_timestamp_get",
+    "date_timestamp_set",
+    "date_timezone_get",
+    "date_timezone_set",
+    "getdate",
+    "gettimeofday",
+    "gmdate",
+    "gmmktime",
+    "gmstrftime",
+    "idate",
+    "localtime",
+    "microtime",
+    "mktime",
+    "strftime",
+    "strptime",
+    "strtotime",
+    "time",
+    "timezone_abbreviations_list",
+    "timezone_identifiers_list",
+    "timezone_location_get",
+    "timezone_name_from_abbr",
+    "timezone_name_get",
+    "timezone_offset_get",
+    "timezone_open",
+    "timezone_transitions_get",
+    "timezone_version_get"
+];}
